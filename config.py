60d2d02b3afe88766fe86728f1c438085c349036
Skye Im <skye.im@nyu.edu>
2019-01-27 10:39:20 +0900

    Feat state: Factor modstates into a plugin

--- config.py
+++ config.py
@@ -1,13 +1,22 @@
 from toothless import commandrouter as cr
 from hello import hello_world
+from state import modstate
 import logging
 import os
 
-
+# By default, discord.py is silent to stdout.
+# Set so all messages are logged
 logging.basicConfig(level=logging.INFO)
 
+
+# The token to authenticate to Discord with
 TOKEN = os.environ['BOTTOKEN']
 
+# All prefix commands must start with this sigil.
+# It must be 0 or 1 characters
+COMMAND_PREFIX = '/'
+
 prefix_patterns = [
-    cr.path('hi', hello_world)
+    cr.path('hi', hello_world),
+    cr.path('state', modstate)
 ]
---
+++ state/__init__.py
@@ -0,0 +1,38 @@
+import re
+import discord
+
+
+MODSTATE_STRING = "Please acknowledge (if appropriate to do so)."
+
+
+async def modstate(client, message, chopped):
+    for role in message.author.roles:
+        if role.name == 'Protectorate':
+            await do_the_state(client, message, chopped)
+            break
+
+
+async def do_the_state(client, message, statecontent):
+    target_channel = message.channel
+    maybematch = re.match(r'<#(\d+)>', statecontent)
+    if maybematch:
+        targetchannelid = maybematch.group(1)
+        target_channel = client.get_channel(targetchannelid)
+        statecontent = statecontent[maybematch.end():]
+
+    pings = set(re.findall(r'<@!*\d+>', statecontent))
+    separator = ' - ' if pings else ''
+    content = f'{" ".join(pings)}{separator}{MODSTATE_STRING}'
+    embed = discord.Embed(
+        title='MOD STATEMENT',
+        timestamp=message.timestamp,
+        type='rich',
+        description=statecontent,
+        color=discord.Color.dark_red()
+    )
+    username = message.author.nick
+    if not username:
+        username = message.author.name
+    avatarurl = message.author.avatar_url
+    embed.set_footer(text=username, icon_url=avatarurl)
+    await client.send_message(target_channel, content=content, embed=embed)
