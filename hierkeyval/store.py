b28c2c57571939248dafdaa692f2c9235aec94f2
Skye Im <skye.im@nyu.edu>
2019-02-14 22:27:30 +0800

    Fix HKV: Add identifier, add namespace wrapper

--- hierkeyval/store.py
+++ hierkeyval/store.py
@@ -9,13 +9,16 @@ The improved version should probably only support custom HDict and HList
 objects to prevent this kind of bug
 """
 class HierarchicalStore:
-    sglobal = {}
-    sserver = {}
-    schannel = {}
+    sglobal = {}  # a 'flat' dict
+    sserver = {}  # Key is namespace, then serv
+    schannel = {}  # Same
     filename = None
     fileobj = None
 
     def __init__(self, filename_or_obj):
+        self.sglobal = {}
+        self.sserver = {}
+        self.schannel = {}
         if isinstance(filename_or_obj, io.StringIO):
             self.fileobj = filename_or_obj
         elif isinstance(filename_or_obj, str):
@@ -41,7 +44,7 @@ class HierarchicalStore:
                 pass  # obj has too many/too few elements
         else:
             raise ValueError('filename_or_obj should be a filename or fileobj')
-        self.dicts = [self.sglobal, self.sserver, self.schannel]
+        self.dicts = {'g': self.sglobal, 's': self.sserver, 'c': self.schannel}
 
     def flush(self):
         if self.filename is None:
@@ -51,7 +54,7 @@ class HierarchicalStore:
             f = self.fileobj
         else:
             f = open(self.filename, 'w')
-        json.dump(self.dicts, f)
+        json.dump([x for x in self.dicts.values()], f)
 
     def has_key(self, namespace, mask, key):
         try:
@@ -60,58 +63,81 @@ class HierarchicalStore:
         except KeyError:
             return False
 
-    def has_namespace(self, namespace, mask):
-        gsc = [x != '-' for x in mask]
-        for i in range(2, -1, -1):
-            if gsc[i]:
-                if namespace in self.dicts[i]:
-                    return True
-        return False
+    def get_dict_and_ident(self, level, identifier):
+        if level == 'g':
+            d = self.sglobal
+            ident = None
+        elif level == 's':
+            d = self.sserver
+            ident = identifier.server
+        elif level == 'c':
+            d = self.schannel
+            ident = identifier.channel
+        else:
+            raise ValueError(f'Param level must be one of g, s, c, but is "{level}".')
+
+        return d, ident
 
     """
-    Mask is format "gsc", where a - means skip that level.
-    So, to only access global, do "g--", to access just global and channel
-    do "g-c", and so on.
+    Checks levels in order from start to end,returning the first level's value found
+    Valid levels are g, s, c for global, server and channel, respectively.
 
     :param namespace: Since HStore is by plugin, specify namespace
-    :param mask: String mask for retrieving keys.
+    :param levels: The levels to check
+    :param identifier: The object to extract server/channel from
     :param key: The key to get the value for.
     """
-    def get_val(self, namespace, mask, key):
-        if len(mask) != 3:
-            raise ValueError('Mask must be len 3')
-        gsc = [x != '-' for x in mask]
-        # iterate in reverse order
-        for i in range(2, -1, -1):
-            if gsc[i]:
-                try:
-                    return self.dicts[i][namespace][key]
-                except KeyError:
-                    pass
+    def get_val(self, levels, namespace, identifier, key):
+        for level in levels:
+            d, ident = self.get_dict_and_ident(level, identifier)
+            try:
+                return d[namespace][ident][key]
+            except KeyError:
+                continue
+            except TypeError:
+                continue
         raise KeyError(f'Could not find {key} in namespace {namespace}.')
 
     """
     Set a value.
 
-    :param namespace: Namespace
     :param level: One of 'g', 's', or 'c', the level to store at
+    :param namespace: Namespace
+    :param identifier: Obj to extract server/channel from
     :param key: The key to store at
     :param val: The value to store
     """
-    def set_val(self, namespace, level, key, val):
-        if level == 'g':
-            d = self.sglobal
-        elif level == 's':
-            d = self.sserver
-        elif level == 'c':
-            d = self.schannel
+    def set_val(self, level, namespace, identifier, key, val):
+        d, ident = self.get_dict_and_ident(level, identifier)
         if namespace not in d:
-            d[namespace] = {}
-        d[namespace][key] = val
+            d[namespace] = {ident: {}}
+        if ident not in d[namespace]:
+            d[namespace][ident] = {}
+        d[namespace][ident][key] = val
         self.flush()
+
+    def as_namespace(self, namespace):
+        return NamespacedHStore(self, namespace)
+
+
+"""
+Convenience wrapper on HSV for get/set that has a namespace set at creation
+"""
+class NamespacedHStore:
+    def __init__(self, HSV, namespace):
+        self.hsv = HSV
+        self.namespace = namespace
+
+    def get_val(self, levels, identifier, key):
+        return self.hsv.get_val(levels, self.namespace, identifier, key)
+
+    def set_val(self, level, identifier, key, val):
+        return self.hsv.set_val(level, self.namespace, identifier, key, val)
 
 
 DEFAULT = HierarchicalStore('default.hkv')
 
-def get_default():
+def get_default(namespace=None):
+    if namespace:
+        return NamespacedHStore(DEFAULT, namespace)
     return DEFAULT
--- hierkeyval/tests/test_store.py
+++ hierkeyval/tests/test_store.py
@@ -2,27 +2,40 @@ from hierkeyval import HierarchicalStore
 import io
 import json
 
+class MockIdentifier:
+    pass
+
+def make_identifier(server, channel):
+    obj = MockIdentifier()
+    obj.server = server
+    obj.channel = channel
+    return obj
+
+
+ident = make_identifier('server', 'channel')
 
 def test_store_value():
     HKV = HierarchicalStore(io.StringIO())
-    HKV.set_val('foo', 'c', 'a', 3)
-    assert HKV.get_val('foo', '--c', 'a') == 3
-    HKV.set_val('foo', 's', 'b', 5)
-    assert HKV.get_val('foo', '-s-', 'b') == 5
-    HKV.set_val('foo', 'g', 'c', 7)
-    assert HKV.get_val('foo', 'g--', 'c') == 7
+    NHKV = HKV.as_namespace('test')
+    NHKV.set_val('c', ident, 'key', 3)
+    assert NHKV.get_val('c', ident, 'key') == 3
+    NHKV.set_val('s', ident, 'key2', 5)
+    assert NHKV.get_val('s', ident, 'key2') == 5
+    NHKV.set_val('g', ident, 'key3', 7)
+    assert NHKV.get_val('g', ident, 'key3') == 7
 
 def test_hierarchical_value():
     HKV = HierarchicalStore(io.StringIO())
-    HKV.set_val('foo', 's', 'key', 10)
-    assert HKV.get_val('foo', 'gsc', 'key') == 10
-    HKV.set_val('foo', 'g', 'key', 20)
-    assert HKV.get_val('foo', 'g-c', 'key') == 20
+    NHKV = HKV.as_namespace('test')
+    NHKV.set_val('s', ident, 'key', 10)
+    assert NHKV.get_val('csg', ident, 'key') == 10
+    NHKV.set_val('g', ident, 'key', 20)
+    assert NHKV.get_val('cg', ident, 'key') == 20
 
 def test_saves_after_write():
     f = io.StringIO()
     HKV = HierarchicalStore(f)
-    HKV.set_val('foo', 's', 'key', 123)
+    HKV.set_val('s', 'test', ident, 'key', 123)
     f.seek(0)
     obj = json.load(f)
-    assert obj[1]['foo']['key'] == 123
+    assert obj[1]['test']['server']['key'] == 123
