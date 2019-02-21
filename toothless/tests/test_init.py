c2b698bde661dbb557ee91e52031c5c4ba870766
Skye Im <skye.im@nyu.edu>
2019-02-21 18:14:33 +0800

    Meta toothless: Move config testing

--- toothless/tests/test_init.py
+++
@@ -1,9 +0,0 @@
-from toothless import Toothless, ConfigError
-import pytest
-
-def try_supply_long_command_prefix():
-    Toothless([], '//')
-
-def test_rejects_long_command_prefixes():
-    with pytest.raises(ConfigError):
-        try_supply_long_command_prefix()
